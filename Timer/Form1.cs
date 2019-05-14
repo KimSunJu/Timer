7d7625a251bd34d06a53020992bfed1cc1fbd88d
Sunju Kim <sunju7420@naver.com>
2019-05-14 12:01:20 +0900

    #2 타이머 코드 구현

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -6,14 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.VisualBasic; // 입력값이 숫자인지를 확인하는 IsNumeric를 위해 추가
 
 namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0; // 초기 카운터
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            if (IntCheck())
+            {
+                CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                txtNum.ReadOnly = true;
+                Timer.Enabled = true;
+            }
+        }
+            private bool IntCheck()
+            {
+                if (Information.IsNumeric(txtNum.Text))
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("숫자를 입력하세요~", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                MessageBox.Show("펑!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                CountOrgNum--;
+                this.txtCountDown.Text = Convert.ToString(CountOrgNum);
+            }
+
+        }
     }
 }
+
